19a9aecf87573bfb58047a85421fbf56965ff99b
Akifumi Fujita <fujita.akifumi@gmail.com>
2019-02-10 23:27:46 +0900

    Fix bug of sltu

--- src/rafi-emu/cpu/Executor.cpp
+++ src/rafi-emu/cpu/Executor.cpp
@@ -683,7 +683,7 @@ void Executor::ProcessAlu(const Op& op)
     const auto src2 = m_pIntRegFile->ReadInt32(operand.rs2);
 
     const auto src1_u = m_pIntRegFile->ReadUInt32(operand.rs1);
-    const auto src2_u = m_pIntRegFile->ReadUInt32(operand.rs1);
+    const auto src2_u = m_pIntRegFile->ReadUInt32(operand.rs2);
 
     int32_t value;
 
