b0921f2be5f52e33552d1a7a5e2e62920fc0f548
Akifumi Fujita <fujita.akifumi@gmail.com>
2019-12-09 00:18:25 +0900

    fix linux build

--- src/rafi-emu/GdbServer.cpp
+++ src/rafi-emu/GdbServer.cpp
@@ -24,8 +24,10 @@
 #include <Windows.h>
 #include <Winsock.h>
 #else
+#include <netinet/ip.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #endif
 
 #include <rafi/emu.h>
@@ -34,6 +36,57 @@
 #include "GdbServer.h"
 
 namespace rafi { namespace emu {
+
+namespace {
+
+template <typename T>
+void ToHex(char* pOutBuffer, size_t bufferSize, const T value)
+{
+    (void)bufferSize; // for release build
+
+    static_assert(std::is_integral_v<T>);
+    static_assert(std::is_unsigned_v<T>);
+
+    auto tmp = value;
+
+    for (int i = 0; i < sizeof(tmp) * 2; i += 2)
+    {
+        const T high = (tmp % 0x100) / 0x10;
+        const T low = tmp % 0x10;
+
+        assert(i + 1 <= bufferSize);
+
+        pOutBuffer[i] = high < 10 ? '0' + high : 'a' + (high - 10);
+        pOutBuffer[i + 1] = low < 10 ? '0' + low : 'a' + (low - 10);
+
+        tmp >>= 8;
+    }
+}
+
+template <>
+void ToHex(char* pOutBuffer, size_t bufferSize, const char* str)
+{
+    (void)bufferSize; // for release build
+
+    for (int i = 0; i < std::strlen(str); i++)
+    {
+        const char high = (str[i] % 0x100) / 0x10;
+        const char low = str[i] % 0x10;
+
+        assert(i * 2 + 1 <= bufferSize);
+
+        pOutBuffer[i * 2] = high < 10 ? '0' + high : 'a' + (high - 10);
+        pOutBuffer[i * 2 + 1] = low < 10 ? '0' + low : 'a' + (low - 10);
+    }
+}
+
+template <>
+void ToHex(char* pOutBuffer, size_t bufferSize, const std::string str)
+{
+    ToHex(pOutBuffer, bufferSize, str.c_str());
+}
+
+}
 
 GdbServer::GdbServer(XLEN xlen, System* pSystem, int port)
     : m_XLEN(xlen)    
@@ -101,7 +154,7 @@ void GdbServer::Process()
     for (;;)
     {
         struct sockaddr_in addr;
-        int addrlen = sizeof(addr);
+        socklen_t addrlen = sizeof(addr);
         const auto clientSocket = ::accept(m_ServerSocket, (struct sockaddr*)&addr, &addrlen);
         if (clientSocket < 0)
         {
--- src/rafi-emu/GdbServer.h
+++ src/rafi-emu/GdbServer.h
@@ -63,53 +63,6 @@ private:
     
     uint64_t ParseHex(const std::string& str);
 
-    template <typename T>
-    void ToHex(char* pOutBuffer, size_t bufferSize, const T value)
-    {
-        (void)bufferSize; // for release build
-
-        static_assert(std::is_integral_v<T>);
-        static_assert(std::is_unsigned_v<T>);
-
-        auto tmp = value;
-
-        for (int i = 0; i < sizeof(tmp) * 2; i += 2)
-        {
-            const T high = (tmp % 0x100) / 0x10;
-            const T low = tmp % 0x10;
-
-            assert(i + 1 <= bufferSize);
-
-            pOutBuffer[i] = high < 10 ? '0' + high : 'a' + (high - 10);
-            pOutBuffer[i + 1] = low < 10 ? '0' + low : 'a' + (low - 10);
-
-            tmp >>= 8;
-        }
-    }
-
-    template <>
-    void ToHex(char* pOutBuffer, size_t bufferSize, const std::string str)
-    {
-        ToHex(pOutBuffer, bufferSize, str.c_str());
-    }
-
-    template <>
-    void ToHex(char* pOutBuffer, size_t bufferSize, const char* str)
-    {
-        (void)bufferSize; // for release build
-
-        for (int i = 0; i < std::strlen(str); i++)
-        {
-            const char high = (str[i] % 0x100) / 0x10;
-            const char low = str[i] % 0x10;
-
-            assert(i * 2 + 1 <= bufferSize);
-
-            pOutBuffer[i * 2] = high < 10 ? '0' + high : 'a' + (high - 10);
-            pOutBuffer[i * 2 + 1] = low < 10 ? '0' + low : 'a' + (low - 10);
-        }
-    }
-
     std::map<paddr_t, uint32_t> m_MemoryBackups;
 
     XLEN m_XLEN;
